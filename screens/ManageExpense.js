b84eb3214b8dda94cdfae61c4c3d842312c8adaa
Avram Alin Ioan <avramalinmedia@gmail.com>
2025-02-25 21:48:06 +0200

    Organized bottom tab group and started styling the recent and all expenses tab

---
+++ screens/ManageExpense.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+function ManageExpense() {
+  return (
+    <View>
+      <Text>ManageExpense Screen</Text>
+    </View>
+  );
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({});
